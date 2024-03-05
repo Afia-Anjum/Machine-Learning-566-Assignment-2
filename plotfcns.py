2916aa52aab6bb9c273c269425d45d4c4b08c4e5
Afia-Anjum <56650056+Afia-Anjum@users.noreply.github.com>
2024-03-05 08:53:10 -0700

    Add files via upload
    MLCourse_functionalities

---
+++ plotfcns.py
@@ -0,0 +1,12 @@
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_histogram(yvalues):
+    plt.hist(yvalues, bins=50, facecolor='green')
+    plt.xlabel('Regression variable')
+    plt.ylabel('Probability')
+    #plt.axis([40, 160, 0, 0.03])
+    plt.grid(True)
+
+    plt.show()
